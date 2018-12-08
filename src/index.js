59efa446e9aef8e5405722371e006a77e8cc5b45
chidi onuekwusi <chidionuekwusi@gmail.com>
2018-12-08 23:55:20 +0100

    exported utils and ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+export { ThemeProvider } from "styled-components";
 export { default as Button } from "./components/common/components/Button";
 export { default as Input } from "./components/Input/input";
 export { default as Checkbox } from "./components/Input/checkbox";
 export { default as DatePicker } from "./components/Input/datepicker";
 export { default as Select } from "./components/select";
+export {
+  injectFontsAndCSSBase,
+  createMedia,
+  media
+} from "./components/common/utils";
 export { default as setup } from "./configure";
